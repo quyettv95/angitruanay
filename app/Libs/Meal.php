26d69d92b30f2e7494ef909b17855db7aa772d83
Quyet Tran <Quyet Tran>
2019-10-11 09:59:30 +0700

    update code

--- app/Libs/Meal.php
+++ app/Libs/Meal.php
@@ -13,7 +13,7 @@ class Meal
             'image_url' => 'https://cdn.24h.com.vn/upload/2-2019/images/2019-04-03/Bun-ca-cay-mon-an-mang-huong-vi-rieng-cua-dat-Cang-1-1554281281-635-width640height427.jpg',
             'description' => 'Bún cá rô đồng là món bún nước với cá rô, có nguồn gốc từ tỉnh Hải Dương thuộc khu vực miền Bắc vùng đồng bằng sông Hồng. Món ăn này chỉ làm từ một loài cá rô sống trên ruộng đồng nhưng khi trải qua bàn tay của người dân quê thuộc tỉnh Hải Dương thì nó đã trở thành một món ăn độc đáo và gây nhiều ấn tượng đối với du khách. Bún cá rô luộc (hấp) được chế biến đơn giản nhưng lại giữ được vị ngọt đặc trưng của thịt cá.',
             'location' => '',
-            'budget' => '30K - 50K / Users',
+            'budget' => '30K - 50K / User',
             'score' => 30
         ],
         [
@@ -22,7 +22,7 @@ class Meal
             'image_url' => 'https://beptruong.edu.vn/wp-content/uploads/2018/06/cach-uop-thit-nuong-com-tam.jpg',
             'description' => 'Bún bò Huế là một trong những đặc sản của xứ Huế, mặc dù món bún này phổ biến trên cả ba miền ở Việt Nam và cả người Việt tại hải ngoại. Tại Huế, món này được gọi đơn giản là "bún bò" hoặc gọi cụ thể hơn là "bún bò giò heo". Các địa phương khác gọi là "bún bò Huế", "bún bò gốc Huế" để chỉ xuất xứ của món ăn này.',
             'location' => '',
-            'budget' => '30K - 50K / Users',
+            'budget' => '30K - 50K / User',
             'score' => 47
         ],
         [
@@ -31,7 +31,7 @@ class Meal
             'image_url' => 'https://cdn.tgdd.vn/Files/2018/07/18/1102275/huong-dan-cach-lam-bun-cha-ha-noi--7.jpg',
             'description' => 'Bún chả là món ăn với bún, chả thịt lợn nướng trên than hoa và bát nước mắm chua cay mặn ngọt. Món ăn xuất xứ từ miền Bắc Việt Nam, là thứ quà có sức sống lâu bền nhất của Hà Nội, nên có thể coi đây là một trong những đặc sản đặc trưng của ẩm thực Hà thành.',
             'location' => '',
-            'budget' => '30K - 50K / Users',
+            'budget' => '30K - 50K / User',
             'score' => 20
         ],
         [
@@ -40,7 +40,7 @@ class Meal
             'image_url' => 'http://streaming1.danviet.vn/upload/3-2017/images/2017-09-09/150493065457643-unnamed--2-.jpg',
             'description' => 'Cơm 19A, ăn 1 lần khiếp 1 tuần',
             'location' => '',
-            'budget' => '30K - 50K / Users',
+            'budget' => '30K - 50K / User',
             'score' => 0
         ],
         [
@@ -49,7 +49,7 @@ class Meal
             'image_url' => 'http://viewdao.net/wp-content/uploads/2018/11/45661534_2173952432865233_1173627915603017728_n.jpg',
             'description' => 'Để hâm nóng tình cảm  anh em và tăng tương tác, đôi khi chúng ta nên tổ chức vài buổi cải thiện có thể là lẩu, buffet, ...',
             'location' => '',
-            'budget' => '100K - 200k / Users',
+            'budget' => '100K - 200k / User',
             'score' => 3
         ],
     ];
